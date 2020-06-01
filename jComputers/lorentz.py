6053b60a76a9aaeb6741176d4e609117e862509f
Kiara <kiarag@alumni.ubc.ca>
2020-06-01 11:15:47 -0700

    made lorentz

---
+++ jComputers/lorentz.py
@@ -0,0 +1,39 @@
+from abc import ABC
+from jComputers.jComputer import JComputer
+
+class Constant(JComputer) :
+  def __init__(self, nnOnly, t, rdep, gamma, energyComputer):
+    """ j_ij = lorentzian(e_i-e_j)/r**3 OR not divided by r**3 if no rdep
+    """ 
+    # avg distance from one particle to next:
+    #		(10^12) particles/cm^3
+    # 1/10^12 cm^3 / particle
+    # 0.5/10^12 cm avg distance between them
+    # so our units will be 1/10^13 cm
+
+    super().__init__(nnOnly)
+    self.t = t  # units are 1/10^13 cm 
+    self.rdep = rdep
+    self.gamma = gamma
+    self.energyComputer = energyComputer
+    if (rdep):
+      self.desc = "j_ij = 	t/ (1 + {(e_i - e_j)/gamma^2) / r_ij**3, \
+      with t = %f, gamma = %f\n" % (t, gamma)
+    else:
+      self.desc = "j_ij = 	t/ (1 + {(e_i - e_j)/gamma^2) \
+      with t = %f, gamma = %f\n" % (t, self.gamma)
+    if (self.nnOnly):
+      self.desc = self.desc + "j_ij on nearest neighbours only\n" 
+
+  def jFinder(self, xi, xj):
+    # should only be called after this instance's energyComputer has had calculateEnergies called
+    if (energyComputer.energies is None):
+      raise Exception("Attempted to use jComputer which needs energy, but energy not yet computed!")
+    if (self.nnOnly and (xi - xj) > 1):
+      return 0
+    r = 5*(xi - xj)
+    denominator = 1 + ((self.energyComputer.energies[i] - self.energyComputer.energies[j])/(self.gamma))**2
+    j = self.t / (np.pi*self.gamma * denominator)
+    if (self.rdep):
+      j = j / (r**3)
+    return j
