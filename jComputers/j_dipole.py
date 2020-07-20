d7bdd33fb93fe03ac222539eb200e507c0fa2c62
Kiara <kiarag@alumni.ubc.ca>
2020-07-20 11:12:04 -0700

    attempted (failed) speed up

--- jComputers/j_dipole.py
+++ jComputers/j_dipole.py
@@ -26,37 +26,38 @@ class J_dipole(JComputer) :
     else:
       self.desc = "j_ij = t*<d_i><d_j>, with t = %f" % (t)
     if (self.nnOnly):
-      self.desc = self.desc + ", j_ij on nearest neighbours only\n" 
+      self.desc = self.desc + ", j_ij on nearest neighbours only\n"
+    if (isinstance(self.energy_computer,  SumOnsitesDecorator)):
+      self.ec = self.energy_computer.inner_ec
+    else:
+      self.ec = self.energy_computer 
+    self.numerov = Numerov()
 
   def jFinder(self, xi, xj):
     # should only be called after this instance's energy_computer has had calculateEnergies called
-    if (isinstance(self.energy_computer,  SumOnsitesDecorator)):
-      ec = self.energy_computer.inner_ec
-    else:
-      ec = self.energy_computer
-    if (self.energy_computer.energies is None):
-      raise Exception("Attempted to use jComputer which needs energy, but energy not yet computed!")
+    # if (self.energy_computer.energies is None):
+    #   raise Exception("Attempted to use jComputer which needs energy, but energy not yet computed!")
     if (self.nnOnly and (xi - xj) > 1):
       return 0      
     r = 5*(xi - xj) 
     
-    n_1i= ec.n0s[xi]
-    l_1i= ec.l0s[xi]
-    n_2i= ec.nfs[xi]
-    l_2i= ec.lfs[xi]
+    n_1i= self.ec.n0s[xi]
+    l_1i= self.ec.l0s[xi]
+    n_2i= self.ec.nfs[xi]
+    l_2i= self.ec.lfs[xi]
     
-    n_1j= ec.n0s[xj]
-    l_1j= ec.l0s[xj]
-    n_2j= ec.nfs[xj]
-    l_2j= ec.lfs[xj]
+    n_1j= self.ec.n0s[xj]
+    l_1j= self.ec.l0s[xj]
+    n_2j= self.ec.nfs[xj]
+    l_2j= self.ec.lfs[xj]
     
     if self.energy_computer.energies[xi]*self.energy_computer.energies[xj]==0:
         j=0
     else:      
-        d_i=Numerov().Rad_int(n_1i,l_1i,n_2i,l_2i) # in atomic unit 
-        d_j=Numerov().Rad_int(n_1j,l_1j,n_2j,l_2j) # in atomic unit     
+        d_i=self.numerov.Rad_int(n_1i,l_1i,n_2i,l_2i) # in atomic unit 
+        d_j=self.numerov.Rad_int(n_1j,l_1j,n_2j,l_2j) # in atomic unit     
         j = self.t*d_i*d_j
-    
+
     if (self.rdep):
       j = j / (r*r*r)
     return j
